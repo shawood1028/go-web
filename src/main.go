d3c20da1207b88720520ee1b87c49bd3844cc3e1
shawood1028 <shawood1028@gmail.com>
2023-12-20 00:09:49 +0800

    333

--- src/main.go
+++ src/main.go
@@ -11,7 +11,7 @@ func main() {
 	r := routes.RegisterRoute()
 	if err := r.Run(); err != nil {
 		fmt.Printf("startup service failed, err:%v\n", err)
-		fmt.Printf("222")
+		fmt.Printf("3333")
 	}
 
 }
