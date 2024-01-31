29806b1a44e7d697fd1d4177a12a1c0961025d2d
shawood1028 <shawood1028@gmail.com>
2024-01-31 23:07:49 +0800

    update

--- routes/route.go
+++ routes/route.go
@@ -8,19 +8,20 @@ import (
 // RegisterRoute 配置路由信息，注册单个路由
 func RegisterRoute() *gin.Engine {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
 	r.GET("/article", articleHandler)
 	r.GET("/", indexHandler)
 	return r
 }
 
 func indexHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
+	ctx.HTML(http.StatusOK, "index.html", gin.H{
 		"msg": "this is index",
 	})
 }
 
 func articleHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
+	ctx.HTML(http.StatusOK, "article.html", gin.H{
 		"msg": "this is article",
 	})
 }
