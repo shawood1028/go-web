1b06741cd4ca68d2c2c4b0501b3739208a2a9f5b
shawood1028 <shawood1028@gmail.com>
2024-02-26 04:08:32 +0800

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-set GOARCH=amd64
-set GOOS=linux
-go build .\src\main.go
-scp -i C:\Users\shawood\Downloads\shawood_alicloud.pem ./main.exe root@47.243.133.65:~
+#!/usr/bin/env bash
+# linux amd64 打包
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./bin/
+scp -i /Users/ohla/Desktop/shawood_alicloud.pem ./bin/main.exe root@47.243.133.65:~/
