c2174e6a3808bc17aefc9b40041b5a7a5600036d
shawood1028 <shawood1028@gmail.com>
2024-03-24 14:37:34 +0800

    update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2023 Start Bootstrap
+* Shawood - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
+* Copyright 2013-2023 Shawood
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
 window.addEventListener('DOMContentLoaded', () => {
